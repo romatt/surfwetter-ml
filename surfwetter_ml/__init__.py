1a0ae2ff235fa323d4bc00528d28c87f3dea2591
Roman <roman@kyril.pro>
2025-05-04 12:31:22 +0000

    Add library settings

--- surfwetter_ml/__init__.py
+++ surfwetter_ml/__init__.py
@@ -1,7 +1,17 @@
 __version__ = "0.1.0"
 
+import os
+from pathlib import Path
+
+from pydantic_yaml import parse_yaml_file_as
+
+from surfwetter_ml.config.setting import LibrarySettings
+
 from .core import compute
 
 __all__ = [
     "compute",
 ]
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG = parse_yaml_file_as(LibrarySettings, Path(ROOT_DIR, "config", "config.yaml"))
--- surfwetter_ml/config/setting.py
+++ surfwetter_ml/config/setting.py
@@ -1,38 +1,31 @@
-import os
-
-import yaml
-from pydantic import BaseModel
-
 from typing import Any
 
-class SubscriptableBaseModel(BaseModel):
+from pydantic import BaseModel
 
+
+class SubscriptableBaseModel(BaseModel):
     def __getitem__(self, item: str) -> Any:
         return getattr(self, item)
 
-class ParamSettings(SubscriptableBaseModel):
-    pass
+
+class ModelSettings(SubscriptableBaseModel):
+    start: int
+    stop: int
+    freq: int
+    distance: float
+
+
+class ModelList(SubscriptableBaseModel):
+    ICON1: ModelSettings
+    ICON2: ModelSettings
 
 
 class NWPSettings(SubscriptableBaseModel):
+    parameters: list[str]
+    model: ModelList
 
-    parameters: list[ParamSettings]
 
-
-class ServiceSettings(BaseModel):
+class LibrarySettings(BaseModel):
     nwp: NWPSettings
-
-    @staticmethod
-    def load(setting_files: list) -> "ServiceSettings":
-        """Load from settings file"""
-        config_data: dict = {}
-        for path in setting_files:
-            with open(os.path.join(os.path.dirname(__file__), path), "r", encoding='UTF-8') as yaml_file:
-                config_data.update(yaml.safe_load(yaml_file))
-        # """Load from environment"""
-        for setting_name in config_data:
-            for setting in config_data[setting_name]:
-                env_var = os.environ.get(f'{setting_name}__{setting}')
-                if env_var:
-                    config_data[setting_name][setting] = env_var
-        return ServiceSettings(**config_data)
+    data: str
+    api: str
