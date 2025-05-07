685943c8b0fa5770198939ac25345ebb25256965
Roman <roman@kyril.pro>
2025-05-07 18:16:14 +0000

    Define logging config in init

--- surfwetter_ml/__init__.py
+++ surfwetter_ml/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 
+import logging
 import os
 from pathlib import Path
 
@@ -12,6 +13,8 @@ from .core import compute
 __all__ = [
     "compute",
 ]
+
+logging.basicConfig(format=" %(name)s :: %(levelname)-8s :: %(message)s", level=logging.INFO)
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG = parse_yaml_file_as(LibrarySettings, Path(ROOT_DIR, "config", "config.yaml"))
